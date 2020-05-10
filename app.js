9bce27f3a6f7a102b45238a0a54ee074c69926e7
Rui Ma <mrsckd3035@gmail.com>
2020-05-10 16:52:05 -0500

    Add an event listener to the input box.

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 const myAPIKey = '2dXx20LqMATZ1L2vsXr';
 const searchPart = document.querySelector('input');
+const searchBox = document.querySelector('form');
+
+searchBox.addEventListener('submit', function(e) {
+  e.preventDefault();
+  search(searchPart.value);
+  searchPart.value = "";
+})
+
+function search(streetName) {
+  
+}

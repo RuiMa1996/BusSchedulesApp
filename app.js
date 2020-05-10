3345f319419391bef8a57ca641b15add6f9c352d
Rui Ma <mrsckd3035@gmail.com>
2020-05-10 18:13:56 -0500

    Get streets information from API website based on user's input

--- app.js
+++ app.js
@@ -9,5 +9,27 @@ searchBox.addEventListener('submit', function(e) {
 })
 
 function search(streetName) {
-  
+  fetch(`https://api.winnipegtransit.com/v3/streets.json?api-key=${myAPIKey}&name=${streetName}&usage=long`)
+    .then(resp => {
+      if(resp.ok) {
+        return resp.json();
+      } else {
+        throw new Error('No streets found');
+      }
+    })
+    .then(nameOfStreet => {
+      let html = "";
+      console.log(nameOfStreet.streets);
+      if(nameOfStreet.streets.length === 0) {
+        console.log('hi');
+       html = "No street found"
+      } else {
+        for(let name of nameOfStreet.streets) {
+          html += `<a href="#" data-street-key=${name.key}>${name.name}</a>`
+        }
+      }
+      listOfSearchResult.innerHTML = "";
+
+      listOfSearchResult.insertAdjacentHTML('afterbegin', html);
+    })
 }

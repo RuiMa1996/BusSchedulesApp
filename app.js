67cff32a759dd3f50b87ae438cc31aa73c20f1e7
Rui Ma <mrsckd3035@gmail.com>
2020-05-11 12:10:49 -0500

    Add a function to display the HTML of table lists.
    One more function to change the time to local time zone.

--- app.js
+++ app.js
@@ -60,19 +60,48 @@ function busSchedules(stop) {
   const busArray = [];
   for (let bus of stop.stops) {
     busArray.push(
-    fetch(`https://api.winnipegtransit.com/v3/stops/${bus.key}/schedule.json?api-key=${myAPIKey}&max-results-per-route=1`)
-      .then(resp => {
-        if (resp.ok) {
-          return resp.json();
-        } else {
-          throw new Error('No stops found');
-        }
-      })
-      .then(busInfo => {
-        for(let busDetails in busInfo) {
-          console.log(busInfo[busDetails]);
-          return busInfo[busDetails];
-        }
-      }))
+      fetch(`https://api.winnipegtransit.com/v3/stops/${bus.key}/schedule.json?api-key=${myAPIKey}&max-results-per-route=1`)
+        .then(resp => {
+          if (resp.ok) {
+            return resp.json();
+          } else {
+            throw new Error('No stops found');
+          }
+        })
+        .then(busInfo => {
+          for (let busDetails in busInfo) {
+            return busInfo[busDetails];
+          }
+        }))
   }
+
+  Promise.all(busArray)
+    .then(result => {
+      let html = '';
+      for (let details of result) {
+        for(let time of details["route-schedules"]) {
+          html += `
+          <tr>
+            <td>${details.stop.street.name}</td>
+            <td>${details.stop["cross-street"].name}</td>
+            <td>${details.stop.direction}</td>
+            <td>${time.route.key}</td>
+            <td>${timeChange(time["scheduled-stops"][0].times.departure.estimated)}</td>
+          </tr>
+        `
+        }
+      }
+      listOfBus.innerHTML = "";
+      listOfBus.insertAdjacentHTML('afterbegin', html);
+    })
+}
+
+function timeChange(timeToChange) {
+  const options = {
+    hour12 : true,
+    hour:  "2-digit",
+    minute: "2-digit"
+  };
+  
+  return new Date(timeToChange).toLocaleTimeString("canada",options);
 }
